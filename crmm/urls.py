c1d6957f9694ed1bd8c6d108f56ef494362abd1f
ABinaya1999 <binayaparajuli17@gmail.com>
2023-10-31 23:44:39 +0545

    authentication

--- crmm/urls.py
+++ crmm/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include('record.urls')),
+    path("auth/",include('login.urls')),
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
